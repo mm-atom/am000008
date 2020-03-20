726600ba8be74a19f556468184f7e171255a5eea
taoqf <tao_qiufeng@126.com>
2020-03-20 09:33:23 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export default function fun() {
+import { Linking } from 'react-native';
+
+/**
+ * 打开支付宝
+ */
+export default function open_alipay(): Promise<boolean> {
+	return Linking.openURL('alipays://');
 }
